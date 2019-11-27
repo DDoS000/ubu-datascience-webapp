85d9e3583ad3876ec5823a69d06d8cfc71b3424e
DDoS000 <suphamongkhon.kh.61@ubu.ac.th>
2019-11-27 15:38:04 +0700

    10%

--- WEBAPP/myapp/views.py
+++ WEBAPP/myapp/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
+import numpy as np
+
+def convert_to_ndarray(x):
+    x = x.replace('[', '')
+    x = x.replace(']', '')
+    y = []
+    for line in x.split('\n'):
+        y.append( list(map(float, line.split())) )
+    return np.array(y)
 
 # Create your views here.
 def matmul(req):
-    a = '12345\n67891'
-    b = '12345\n67891\n98764'
+    a = convert_to_ndarray(' 1 2 3\n6 7 7\n8 9 6')
+    b = convert_to_ndarray(' 1 2 3\n6 7 7\n8 9 6')
     if req.method == 'POST':
-        print('POST เด้อ')
-        print(req.POST)
+        a = convert_to_ndarray(req.POST['A'])
+        b = convert_to_ndarray(req.POST['B'])
     else:
-        print('GET เด้อ')
+        pass
     return render(req, 'myapp/matmul.html',{
-    'A':a,
-    'B':b
+    'A': a,
+    'B': b,
+    'C': np.dot(a,b)
     })
+
